02920cb7044107a22c4b17a3fa092570e64f68d8
Heorhii <george.luzhan@gmail.com>
2024-02-26 16:31:42 +0200

    some tests with blazor functionality

---
+++ Tangy_Models/TestProduct.cs
@@ -0,0 +1,11 @@
+﻿namespace Tangy_Models
+{
+    public class TestProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+		public List<TestProductProp> ProductProperties { get; set; }
+	}
+}
